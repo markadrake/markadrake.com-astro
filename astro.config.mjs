ebb8629df80a2ae0620d53c921720cd435b0ddef
Mark Drake <markusdrake@gmail.com>
2023-05-22 08:50:10 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify/functions";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://markadrake.com",
+	integrations: [mdx(), sitemap()],
+	output: "server",
+	adapter: netlify()
+});
